0714ffaf6d557d4ae7aaedf42085107f5f60d584
gwh120104 <exu@ai.codes>
2017-04-27 16:28:52 -0400

    Updated config.py

--- config.py
+++ config.py
@@ -4,35 +4,31 @@ config = {
 		{
 			'gene_expression_maximum_level': 10.0,
 			'gene_expression_minimum_level': -10.0,
+			'phenotype_scale_factor': 1.0,
+		},
+	'model_construct':
+		{
+			'gene_protein_connection_mask': 'toy_model/toy_gene_protein_rule.csv',
+			'protein_phenotype_connection_mask': 'toy_model/toy_protein_phenotype_rule_growth.csv',
+
 		},
 	'unsupervised':
 		{
 			'gene': 'toy_model/toy_unsupervised_transcriptomics.csv',
 			'epochs': 200,
 			'batch_size': 5000,
-			'save_to': 'toy_model/your_toy_unsupervised_result.csv'
+			'save_to': 'toy_model/your_toy_unsupervised_result.csv',
 		},
 
 	'supervised':
 		{
 			'gene': 'toy_model/toy_supervised_transcriptomics.csv',
 			'phenotype': 'toy_model/toy_supervised_phenotype_growth.csv',
+			'epochs': 100000,
+			'converge_delta': 0.0001,
 			'indices': 'toy_model/toy_phenotype_indices.txt',
+			'save_to': 'toy_model/your_toy_supervised_result.csv',
 		},
 
 }
 
-"""
-gene_protein_connection_mask
-connection_mast
-flags.DEFINE_string('protein_phenotype_mask', 'pro_pheno_rule_1.csv',                                                                                           │····················
-
-unsupervised_learning:
-
-flags.DEFINE_string('up_save_to', '', 'File to save unsupervised predictions to')                                                                               │····················
-
-supervised:
-
-flags.DEFINE_string('sup_save_to', '', 'File to save supervised predictions to')                                                                                │····················
-
-"""
